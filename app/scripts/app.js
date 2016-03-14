5a2c3b1e1346808c5ad8faa7bfaff392cc4dd4e8
Nikhil Nanjappa <nnanjappa@Nikhils-MacBook-Pro.local>
2016-03-14 21:56:26 +0530

    uploading project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name seeApp
+ * @description
+ * # seeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('seeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name seeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the seeApp
+ */
+angular.module('seeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
