dfab16d56350093a9b2ca98f7b78fe70bb50d646
Nikhil Nanjappa <nnanjappa@Nikhils-MacBook-Pro.local>
2016-03-15 23:49:04 +0530

    made few svg contents dynamic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,11 @@
  * Controller of the seeApp
  */
 angular.module('seeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+  	$scope.values = {
+  		"sme_num": "abc345",
+  		"discount_rate": "19"
+  	}
+
   });
